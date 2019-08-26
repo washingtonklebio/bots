82895cc67240d98c7aff685315823352f6efaffb
Washington <washington@localhost.localdomain>
2019-08-26 12:59:06 -0300

    Event Implementation

--- bot/bot1.js
+++ bot/bot1.js
@@ -4,9 +4,45 @@ const Telegraf = require('telegraf')
 const bot = new Telegraf(env.token)
 
 bot.start(ctx => {
-    const from = ctx.update.message.from
-    console.log(from)
-    ctx.reply(`Seja bem vindo, ${from.first_name}!`)
+    const name = ctx.update.message.from.first_name
+    ctx.reply(`Seja bem vindo, ${name}!`)
+})
+
+bot.on('text', ctx => {
+    const text = ctx.update.message.text
+    ctx.reply(`Texto '${text}' recebido com sucesso!`)
+})
+
+bot.on('location', ctx => {
+    const location = ctx.update.message.location
+    ctx.reply(`Entendido, você está em 
+        latitude: ${location.latitude}
+        longitude: ${location.longitude}`
+    )
+})
+
+bot.on('contact', ctx => {
+    const contact = ctx.update.message.contact
+    const name = contact.first_name
+    const phone = contact.phone_number
+    ctx.reply(`Vou lembrar do(a) ${name} (${phone})`)
+})
+
+bot.on('voice', ctx => {
+    const voice = ctx.update.message.voice
+    ctx.reply(`Audio recebido, ele possui ${voice.duration} segundos`)
+})
+
+bot.on('photo', ctx => {
+    const photo = ctx.update.message.photo
+    photo.forEach((ph, i) => {
+        ctx.reply(`Foto ${i} tem resolução de ${ph.width}x${ph.height}`)
+    });
+})
+
+bot.on('sticker', ctx => {
+    const sticker = ctx.update.message.sticker
+    ctx.reply(`Você enviou o ${sticker.emoji} do conjunto ${sticker.set_name}`)
 })
 
 bot.startPolling()
